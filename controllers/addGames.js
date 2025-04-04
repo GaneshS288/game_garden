a376836ada7e477a57d7ca99fcf66b38e20a2eef
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-04-04 12:41:12 +0530

    feat: allow adding new games via form

---
+++ controllers/addGames.js
@@ -0,0 +1,25 @@
+import DB from "../db/queries.js";
+import asyncHandler from "../lib/asyncHandler.js";
+import { validationResult } from "express-validator";
+
+const renderAddGameForm = asyncHandler(async (req, res, next) => {
+  const genres = await DB.getAllGenres();
+
+  res.status(200).render("newGameForm.ejs", { genres });
+});
+
+const addGame = asyncHandler(async (req, res, next) => {
+  const result = validationResult(req);
+
+  if(result.isEmpty()) {
+    const {title, developer, publisher, genre } = req.body;
+    const game = await DB.addGame({title, developer, publisher, genre});
+    res.status(201).redirect("/games/all");
+  }
+  else {
+    console.log(result.array());
+    res.send(result.array()[0].message);
+  }
+})
+
+export { renderAddGameForm, addGame };
--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -1,13 +1,32 @@
-import { Router } from "express";
+import express, { Router } from "express";
 import { getAllGames, getGamesByGenre } from "../controllers/getGames.js";
-import { deleteAllGames, deleteGameByTitle, deleteGamesByGenre } from "../controllers/deleteGames.js";
+import {
+  deleteAllGames,
+  deleteGameByTitle,
+  deleteGamesByGenre,
+} from "../controllers/deleteGames.js";
+import { addGame, renderAddGameForm } from "../controllers/addGames.js";
+import { body } from "express-validator";
 
-const gamesRouter = new Router;
+const gamesRouter = new Router();
 
+gamesRouter.use(express.urlencoded({ extended: true }));
+
+gamesRouter.get("/new", renderAddGameForm);
 gamesRouter.get("/all", getAllGames);
 gamesRouter.get("/all/delete", deleteAllGames);
 gamesRouter.get("/:genre", getGamesByGenre);
 gamesRouter.get("/:genre/delete", deleteGamesByGenre);
 gamesRouter.get("/:genre/delete/:title", deleteGameByTitle);
+
+gamesRouter.post(
+  "/new",
+  body(["title", "developer", "publisher", "genre"])
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage("this field can't be empty!"),
+  addGame
+);
 
 export default gamesRouter;
